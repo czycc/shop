ea2410d942ba5fda689200089ee49d7a00b8d414
zhougogo <1290612478@qq.com>
2017-08-25 15:37:37 +0800

    上传loading页

--- resources/views/shop/loading.blade.php
+++ resources/views/shop/loading.blade.php
@@ -36,10 +36,10 @@
     },15)
     var imgarr = [];
     for(var i = 1 ;i < 10;i ++){
-        imgarr.push('vip/images/loading/ladingpage/loding page_0000'+i+'.png')
+        imgarr.push('../vip/images/loading/ladingpage/loding page_0000'+i+'.png')
     }
     for(var i = 10 ;i < 69;i ++){
-        imgarr.push('vip/images/loading/ladingpage/loding page_000'+i+'.png')
+        imgarr.push('../vip/images/loading/ladingpage/loding page_000'+i+'.png')
     }
 
     var frame2 = new SequenceFrame({
