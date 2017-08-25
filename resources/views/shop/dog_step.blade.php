edccd476b1d4f1fa3ef18c5144f60bb10d22604e
zhaoyang <602155776@qq.com>
2017-08-25 11:28:30 +0800

    第一版 修改 正式上线 bug

--- resources/views/shop/dog_step.blade.php
+++ resources/views/shop/dog_step.blade.php
@@ -14,70 +14,70 @@
 <a href="{{ url('shop/index') }}" class="home"><img src="{{ asset('vip/images/dog_step/home.png') }}"></a>
 <a href="javascript:void(0)" class="btn"><img src="{{ asset('vip/images/dog_step/btn.png') }}"></a>
 
-	<!-- mask start -->
-	<div class="mask mask_store hidden" id="mask">
-		<div class="show_store">
-			<h1>门店信息</h1>
-			<ul>
-				<li>成都:双楠伊藤HP</li>
-				<li>成都:成仁伊藤HP</li>
-				<li>成都:春熙伊藤HP</li>
-				<li>成都:建设路伊藤HP</li>
-				<li>成都:高新伊藤HP</li>
-				<li>温江:伊藤HP</li>
-				<li>重庆:江北世纪新都综合HP</li>
-				<li>重庆:沙坪凯瑞商都综合HP</li>
-				<li>重庆:杨家坪新世纪HP</li>
-				<li>重庆:大坪时代天街A馆店HP</li>
-				<li>成都:新南凯德广场店HP</li>
-				<li>上海:龙之梦店HP</li>
-				<li>上海:虹口龙之梦店HP</li>
-				<li>上海:港汇广场店HP</li>
-				<li>上海:闵行七宝万科城市广场店HP</li>
-				<li>上海:月星环球港店HP</li>
-				<li>上海:长泰广场店HP</li>
-				<li>上海:紫荆国际广场店HP</li>
-				<li>杭州:申花银泰置地店HP</li>
-				<li>杭州:西溪印象城店HP</li>
-				<li>北京:汉光HP</li>
-				<li>北京:龙德翠微HP</li>
-				<li>北京:世纪金源HP</li>
-				<li>北京:新世界一期HP</li>
-				<li>北京:恒泰广场店HP</li>
-				<li>北京:龙湖天街大兴HP</li>
-				<li>石家庄:人民HP</li>
-				<li>石家庄:北国商城HP</li>
-				<li>天津:金元宝滨海国际HP</li>
-				<li>天津:友谊新天地HP</li>
-				<li>呼市维多利HP</li>
-				<li>太原茂业天地HP</li>
-				<li>秦皇岛:现代购物广场HP</li>
-				<li>秦皇岛:茂业HP</li>
-				<li>唐山:万达百货HP</li>
-			</ul>
-            <img src="{{ asset('vip/images/dog_step/dog.png') }}" class="dog">
-		</div>
-	</div>
+<!-- mask start -->
+<div class="mask mask_store hidden" id="mask">
+    <div class="show_store">
+        <h1>门店信息</h1>
+        <ul>
+            <li>成都:双楠伊藤HP</li>
+            <li>成都:成仁伊藤HP</li>
+            <li>成都:春熙伊藤HP</li>
+            <li>成都:建设路伊藤HP</li>
+            <li>成都:高新伊藤HP</li>
+            <li>温江:伊藤HP</li>
+            <li>重庆:江北世纪新都综合HP</li>
+            <li>重庆:沙坪凯瑞商都综合HP</li>
+            <li>重庆:杨家坪新世纪HP</li>
+            <li>重庆:大坪时代天街A馆店HP</li>
+            <li>成都:新南凯德广场店HP</li>
+            <li>上海:龙之梦店HP</li>
+            <li>上海:虹口龙之梦店HP</li>
+            <li>上海:港汇广场店HP</li>
+            <li>上海:闵行七宝万科城市广场店HP</li>
+            <li>上海:月星环球港店HP</li>
+            <li>上海:长泰广场店HP</li>
+            <li>上海:紫荆国际广场店HP</li>
+            <li>杭州:申花银泰置地店HP</li>
+            <li>杭州:西溪印象城店HP</li>
+            <li>北京:汉光HP</li>
+            <li>北京:龙德翠微HP</li>
+            <li>北京:世纪金源HP</li>
+            <li>北京:新世界一期HP</li>
+            <li>北京:恒泰广场店HP</li>
+            <li>北京:龙湖天街大兴HP</li>
+            <li>石家庄:人民HP</li>
+            <li>石家庄:北国商城HP</li>
+            <li>天津:金元宝滨海国际HP</li>
+            <li>天津:友谊新天地HP</li>
+            <li>呼市维多利HP</li>
+            <li>太原茂业天地HP</li>
+            <li>秦皇岛:现代购物广场HP</li>
+            <li>秦皇岛:茂业HP</li>
+            <li>唐山:万达百货HP</li>
+        </ul>
+        <img src="{{ asset('vip/images/dog_step/dog.png') }}" class="dog">
+    </div>
+</div>
 
-	<div class="mask mask_relation hidden" id="mask2">
-		<form class="show_relation" action="{{ url('shop/relate') }}" method="post">
-			{!! csrf_field() !!}
-			<p class="relation_title">请将狗狗项圈屏幕上的代码输入下面框框</p>
-			<p class="relation_input"><input type="text" name="mac" placeholder="请写代码"></p>
-			<label class="relation_btn" >
-				<img src="{{ asset('vip/images/dog_step/confirm.png') }}">
-				<input type="submit" style="display: none">
-			</label>
-			
-		</form>
-	</div>
-	<!-- mask end -->
+<div class="mask mask_relation hidden" id="mask2">
+    <form class="show_relation" action="{{ url('shop/relate') }}" method="post">
+        {!! csrf_field() !!}
+        <p class="relation_title">请将狗狗项圈屏幕上的代码输入下面框框</p>
+        <p class="relation_input"><input type="text" name="mac" placeholder="请写代码"></p>
+        <label class="relation_btn">
+            <img src="{{ asset('vip/images/dog_step/confirm.png') }}">
+            <input type="submit" style="display: none">
+        </label>
+
+    </form>
+</div>
+<!-- mask end -->
 
 <!-- script -->
 <script type="text/javascript" src="{{ asset('vip/js/jquery.min.js') }}"></script>
 <script type="text/javascript">
     //提示错误信息
-    @if(isset(session('error')))
+    @if(!is_null(session('error')))
     alert('{{ session('error') }}');
     @endif
 
@@ -87,32 +87,32 @@
             $('.mask_store').show();
         })
 
-	    	$('.btn').click(function(e){
-	    		$('.mask_relation').show();
-	    	})
+        $('.btn').click(function (e) {
+            $('.mask_relation').show();
+        })
 
-	    	//点击弹窗消失
-	    	$('.mask').click(function(e){
-	    		if(e.target.id === "mask" || e.target.id === "mask2"){
-	    			$(this).hide();
-	    		}
-	    	});
+        //点击弹窗消失
+        $('.mask').click(function (e) {
+            if (e.target.id === "mask" || e.target.id === "mask2") {
+                $(this).hide();
+            }
+        });
 
-            //表单验证
-            $("form").submit(function(){
-                var text = $(this).val();
-                var reg = /^[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]$/;
-                if(!reg.test(text)){
-                    alert('请填写正确的MAC地址(冒号必须在英文状态下输入)')
-                    return false;
-                }else{
-                    return true;
-                }
-            });
+        //表单验证
+        $("form").submit(function () {
+            var text = $(this).val();
+            var reg = /^[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]$/;
+            if (!reg.test(text)) {
+                alert('请填写正确的MAC地址(冒号必须在英文状态下输入)')
+                return false;
+            } else {
+                return true;
+            }
+        });
 
-    	})
+    })
 
-    	
-    </script>
+
+</script>
 </body>
 </html>
