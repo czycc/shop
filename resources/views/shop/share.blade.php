baf8432110858bae9aa8a97d6fd8001130a96c9f
zhaoyang <602155776@qq.com>
2017-09-18 18:00:20 +0800

    后台统计

--- resources/views/shop/share.blade.php
+++ resources/views/shop/share.blade.php
@@ -1,5 +1,5 @@
-<script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
 <script src="{{asset('vip/js/jquery.min.js')}}"></script>
+<script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="application/javascript">
     wx.config(<?php echo $js->config(array('onMenuShareTimeline', 'onMenuShareAppMessage'), false) ?>);
     // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在 页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready 函数中。
@@ -10,7 +10,6 @@
             link: "https://weixin.touchworld-sh.com/shop",
             imgUrl: "{{asset('vip/images/share.jpg')}}", // 分享图标
             success: function () {
-                $.post({{ url('api/shop/share') }})
             }
         });
         // 获取“分享给朋友”按钮点击状态及自定义分享内容接口
@@ -21,7 +20,6 @@
             imgUrl: "{{asset('vip/images/share.jpg')}}", // 分享图标
             type: 'link', // 分享类型,music、video或link，不填默认为link
             success: function () {
-                $.post({{ url('api/shop/share') }})
             }
         });
     });
