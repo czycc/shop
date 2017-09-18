5ba5adfa16f0e533e00f84baa94a7e639ac06894
LOLO <279144728@qq.com>
2017-09-18 14:20:40 -0700

    add file

--- public/jl/js/sequenceFrame.js
+++ public/jl/js/sequenceFrame.js
@@ -1,56 +1,78 @@
-function SequenceFrame(options){
-	this.options = {
-		id: options.id,
-		width: options.width,
-		height: options.height,
-		speed: options.speed,
-		loop: options.loop,
-		imgArr: options.imgArr,
-		callback: options.callback
-	};
-	this.source = {};
-	this.index = 1;
-	this.imgNum = 1;
-	this.timer = null;
-	this.imgLoad();
+function SequenceFrame(options) {
+    //获取参数
+    this.options = {
+        id: options.id,
+        width: options.width,
+        height: options.height,
+        speed: options.speed,
+        loop: options.loop,
+        imgArr: options.imgArr,
+        autoplay: options.autoplay,
+        callback: options.callback
+    };
+
+    //初始化参数
+    this.source = {};
+    this.index = 1;
+    this.imgNum = 1;
+    this.timer = null;
+    this.canvas = this.options.id;
+    this.ctx = this.canvas.getContext('2d');
+
+    //加载图片
+    this.imgLoad();
 }
 
 SequenceFrame.prototype = {
-	constructor : SequenceFrame,
+    constructor: SequenceFrame,
+    
+    imgLoad: function () {
+        this.source[this.index] = new Image();
+        this.source[this.index].src = this.options.imgArr[this.index - 1];
+        this.source[this.index].onload = function () {
+            this.index++;
+            if (this.index > this.options.imgArr.length) {
+                if(this.options.autoplay == undefined || this.options.autoplay){
+                    this.render();
+                }
+            } else {
+                this.imgLoad();
+            }
+        }.bind(this);
+    },
 
-	imgLoad: function(){
-		this.source[this.index] = new Image();
-		this.source[this.index].src = this.options.imgArr[this.index-1];
-		this.source[this.index].onload = function(){
-			this.index++;
-			if(this.index > this.options.imgArr.length){
-				this.render();
-			}else{
-				this.imgLoad();
-			}
-		}.bind(this);
-	},
+    render: function () {
+        clearInterval(this.timer);
+        this.timer = setInterval(this.playing.bind(this).bind(this), this.options.speed);
+    },
 
-	render: function(){
-		var canvas = this.options.id;
-		var ctx = canvas.getContext('2d');
-		var imgLength = this.options.imgArr.length;
-		canvas.width = this.options.width;
-		canvas.height = this.options.height;
-		clearInterval(this.timer);
-		this.timer = setInterval(function(){
-			if(this.imgNum == imgLength && this.options.loop){
-				this.imgNum = 1;
-			}else if(this.imgNum == imgLength && !this.options.loop){
-				clearInterval(this.timer);
-				if(typeof this.options.callback === 'function'){
-                    this.options.callback()
-				}
-			}else{
-				ctx.clearRect(0,0,this.options.width,this.options.height);
-            	ctx.drawImage(this.source[this.imgNum],0,0,this.options.width,this.options.height);
-            	this.imgNum++;
-			}
-		}.bind(this),this.options.speed);
-	}
+    playing: function () {
+        //计时器绘图函数
+        var imgLength = this.options.imgArr.length;
+        this.canvas.width = this.options.width;
+        this.canvas.height = this.options.height;
+        if (this.imgNum == imgLength && this.options.loop) {
+            //循环播放
+            this.imgNum = 1;
+        } else if (this.imgNum == imgLength && !this.options.loop) {
+            //播放一次后调用回调函数
+            clearInterval(this.timer);
+            if (typeof this.options.callback === 'function') {
+                this.options.callback()
+            }
+        }
+        //将图片绘制到canvas上
+        this.ctx.clearRect(0, 0, this.options.width, this.options.height);
+        this.ctx.drawImage(this.source[this.imgNum], 0, 0, this.options.width, this.options.height);
+        this.imgNum++;
+    },
+
+    //可调用的API
+    play: function () {
+        //停止所有播放,手动调用播放
+        clearInterval(this.timer);
+        this.imgNum = 1;
+        this.timer = setInterval(this.playing.bind(this), this.options.speed);
+    },
+
 }
