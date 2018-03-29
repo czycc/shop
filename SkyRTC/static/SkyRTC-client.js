2c7a11c536f04244d7edbc8b5e61664c0d8642d3
LOLO <279144728@qq.com>
2018-03-29 14:31:16 +0800

    rtc

--- SkyRTC/static/SkyRTC-client.js
+++ SkyRTC/static/SkyRTC-client.js
@@ -7,7 +7,7 @@ var SkyRTC = function() {
     var moz = !!navigator.mozGetUserMedia;
     var iceServer = {
         "iceServers": [
-            {url: 'stun.ideasip.com'}
+            {url: 'stun.ideasip.com:3478'}
         ]
     };
     var packetSize = 1000;
