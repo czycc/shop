c7e4e2e350e74dee28f718cbf358aaea5a93fa82
zhougogo <1290612478@qq.com>
2018-04-03 13:21:01 +0800

    会员商城

--- resources/views/shop/coupon.blade.php
+++ resources/views/shop/coupon.blade.php
@@ -26,49 +26,43 @@
     <a href="{{ url('shop/draw') }}" class="btn"><img src="{{asset('vip/images/coupon/btn.png')}}"></a>
     <a href="{{ url('shop/index') }}" class="home"><img src="{{asset('vip/images/coupon/home.png')}}"></a>
     <ul class="cjText">
-        {{--循环输出优惠券信息，NO.最多6位数--}}
-        @foreach($tickets as $ticket)
-            <li>
-                <div>
-                    <p class="textNum">NO:<span>{{ $ticket->ticket }}</span></p>
-                    <p class="dataText">有效期:<span>{{ $ticket->end }}</span></p>
-                </div>
-                <img src="{{asset('vip/images/coupon/hundred.png')}}" alt="">
-            </li>
-        @endforeach
+        <li>
+            <div class="img">
+                <img src="{{asset('vip/images/coupon/logo.png')}}" alt="">
+            </div>
+            <div>
+                <p class="textNum">NO:<span>773221</span></p>
+                <p class="dataText">有效期:<span>2017.08.20</span></p>
+            </div>
+        </li>
+        <li>
+            <div class="img">
+                <img src="{{asset('vip/images/coupon/logo1.png')}}" alt="">
+            </div>
+            <div>
+                <p class="textNum">NO:<span>773221</span></p>
+                <p class="dataText">有效期:<span>2017.08.20</span></p>
+            </div>
+        </li>
+        <li>
+            <div class="img">
+                <img src="{{asset('vip/images/coupon/logo2.png')}}" alt="">
+            </div>
+            <div>
+                <p class="textNum">NO:<span>773221</span></p>
+                <p class="dataText">有效期:<span>2017.08.20</span></p>
+            </div>
+        </li>
     </ul>
     <img src="{{asset('vip/images/coupon/rule.png')}}" alt="" class="ruleBtn">
     <div class="popup">
         <ul class="ruleText">
-            <li>1.适用于中国内地所有暇步士门店</li>
-            <li>2.适用于暇步士2017秋冬电脑牌价7折</li>
-            <li>（不含）以上新品，以下新品狗仔鞋不参与活动：</li>
-            <li class="ma"> CZPHAV33FU1CM7、CZPHAV47FM8CM7、</li>
-            <li class="ma"> CZPHAV47FR3CM7、CZPHAV47FU1CM7、</li>
-            <li class="ma"> CZPHFB38FU1CM7、CZPHFB38FU4CM7、</li>
-            <li class="ma"> CZPHLX31FD1CM7、CZPHLX31FU1CM7、</li>
-            <li class="ma"> CZPHLX38FK1CM7、CZPHLX38FL5CM7、</li>
-            <li class="ma"> CZPHLX38FU1CM7、CZPHMB20FM2CM7、</li>
-            <li class="ma"> CZPHMB20FP1CM7、CZPHMB20FU1CM7。</li>
-            <li>3.每双鞋只能使用一张优惠券</li>
-            <li>4.派券时间:2017年9月15日~10月31日</li>
-            <li>5.优惠券有效期：2017年9月15日~10月31日</li>
-            <li>6.新百丽鞋业（深圳）有限公司有权在法律允许范围内对本活动解释</li>
-            <li class="maText" style="text-align: center">使用优惠券还可参与抽奖</li>
-            <li style="text-align: center">抽奖规则如下：</li>
-            <li>1.开奖时间：2017年11月15日</li>
-            <li>2.奖品包括：</li>
-            <li class="ma">魔声耳机：价值3200元（共2名）</li>
-            <li class="ma">暇步士鞋履：价值1000元（共4名））</li>
-            <li class="ma">暇步士8寸狗公仔：价值400元（共20名）</li>
-            <li>3.开奖平台：关注暇步士HushPuppies官方微博（http://weibo.com/1958hushpuppies）和暇步士官方微信（HushPuppiesChina）</li>
-            <li style="margin-top: 68px">4.通知领奖时间和方式：开奖后15个工作日内以电话、短信方式通知，凭优惠券验证码和手机号领奖，请妥善保管</li>
-            <li style="margin-top: 64px;">5.兑奖时间：开奖日起45个工作日内兑奖有效，超过兑奖时间未领奖视为放弃获奖资格</li>
-            <li style="margin-top: 32px">6.获奖奖品不得转让、更换且不得换算为现金</li>
-            <li>7.如预留的联系方式或提交的兑奖信息有误、不符合要求而造成无法兑奖等后果，需由获奖用户自行承担责任</li>
+            <li>1、此券仅限酷迪珠江店使用，店铺地址：北京市朝阳区珠江帝景商业南街b1酷迪宠物;</li>
+            <li>2、可使用项目为：基础水疗SPA、特效SPA、果蔬SPA、泥疗SPA、体膜SPA使用；</li>
+            <li>3、可使用日期为2018年4月1日至2018年5月31日，周六日及节假日可用；</li>
+            <li>4、不可叠加使用，每次仅限使用一张，不可折现，不可找零，不与其他优惠同时使用。</li>
         </ul>
     </div>
-
 </div>
 
 </body>
